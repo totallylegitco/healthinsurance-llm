55223c085b7023e0e938e39a73f8451e80497bff
Holden Karau <holden@pigscanfly.ca>
2023-06-02 16:55:49 -0700

    For bits and bytes we need to build from source some of the time (incl jetson)

--- run.sh
+++ run.sh
@@ -5,11 +5,18 @@ set -ex
 INPUT_MODEL=${INPUT_MODEL:-"databricks/dolly-v2-3b"}
 TR_DATA=${TR_DATA:-"out"}
 OUTDIR=${OUTDIR:-"new_model"}
+# On Jetson AGX we need to add /usr/local/cuda/bin to our path
+if [ -f /usr/local/cuda/bin/nvcc ]; then
+  export PATH=$PATH:/usr/local/cuda/bin
+fi
 
 if [ ! -d dolly ]; then
   git clone https://github.com/databrickslabs/dolly.git
   pip install -r ./dolly/requirements.txt
 fi
+
+# Check bits and bytes, it needs to be compiled from source for the jetson (and some others)
+python -m bitsandbytes || ./setup_bits_and_bytes.sh
 if [ ! -d out ]; then
   mkdir out
   python dataset_generator.py
---
+++ setup_bits_and_bytes.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -ex
+
+if [ ! -d bitsandbytes ]; then
+  git clone https://github.com/TimDettmers/bitsandbytes.git
+fi
+cd bitsandbytes
+BUILD_COMMAND=$(python -m bitsandbytes 2>&1 |grep "make" |grep "CUDA_VERSION" |grep -v rm |tail -n 1)
+`$BUILD_COMMAND`
+cd ..
