b0dac26ab1922cc8512c058a1c06ed4fc58e0789
Holden Karau <holden@pigscanfly.ca>
2023-06-03 11:35:42 -0700

    Don't use quantization for testing the new model and also (I know it's bad to use our train data for test but just for introspection) print out what the new model looks like.
    Fix
    
    Fix
    
    Fix

--- dataset_generator.py
+++ dataset_generator.py
@@ -13,16 +13,26 @@ data_files = [f for f in listdir(raw_dataset) if (isfile(join(raw_dataset, f)))]
 
 rejection_files = [f for f in data_files if f.endswith("_rejection.txt")]
 
+def make_appeal_name(rejection, index):
+    if index == 0:
+        return rejection.replace("rejection", "appeal")
+    else:
+        return rejection.replace("rejection", f"appeal{i}")
+
 with open("out/out.jsonl", "w") as o:
     for f in rejection_files:
         with open(join(raw_dataset, f)) as r: rejection = r.read()
-        with open(join(raw_dataset, f.replace("rejection", "appeal"))) as a: appeal = a.read()
-        prompt = f"{header}{rejection}"
-        record = json.dumps({
-            "instruction": prompt,
-            "context": "",
-            "response": appeal,
-            "category": category})
-        record.replace("\n", "")
-        o.write(record)
-        o.write("\n")
+        for i in range(0, 1000):
+            try:
+                with open(join(raw_dataset, make_appeal_name(f, i))) as a: appeal = a.read()
+                prompt = f"{header}{rejection}"
+                record = json.dumps({
+                    "instruction": prompt,
+                    "context": "",
+                    "response": appeal,
+                    "category": category})
+                record.replace("\n", "")
+                o.write(record)
+                o.write("\n")
+            except FileNotFoundError:
+                break
--- test_new_model.py
+++ test_new_model.py
@@ -1,38 +1,25 @@
 from transformers import (
     AutoModelForCausalLM,
-    AutoTokenizer)
+    AutoTokenizer,
+    pipeline)
 from dolly.training.generate import generate_response
-from transformers import BitsAndBytesConfig
 import torch
-from peft import prepare_model_for_kbit_training
-from peft import LoraConfig, get_peft_model
+from os import listdir
+from os.path import isfile, join
 
 
-instructions = [
-    "Write a health insruance appeal."
-]
-
-pretrained_model_name_or_path = "./dolly/new_model"
-
-tokenizer = AutoTokenizer.from_pretrained(pretrained_model_name_or_path, padding_side="left")
-
-quantization_config = BitsAndBytesConfig(
-    load_in_4bit=True,
-    bnb_4bit_compute_dtype=torch.bfloat16,
-    bnb_4bit_quant_type="nf4",
-    bnb_4bit_use_double_quant=True,
-)
-
-model = AutoModelForCausalLM.from_pretrained(
-    pretrained_model_name_or_path, device_map="auto", torch_dtype=torch.bfloat16, trust_remote_code=True,
-    quantization_config=quantization_config,
-)
+instruct_pipeline = pipeline(model="./dolly/new_model", torch_dtype=torch.bfloat16, trust_remote_code=True, device_map="auto")
+old_instruct_pipeline = pipeline(model="databricks/dolly-v2-7b", torch_dtype=torch.bfloat16, trust_remote_code=True, device_map="auto")
 
 
-model, tokenizer = load_model_tokenizer_for_generate(dbfs_output_dir)
+raw_dataset = "appeals-llm-data"
 
-# Use the model to generate responses for each of the instructions above.
-for instruction in instructions:
-    response = generate_response(instruction, model=model, tokenizer=tokenizer, **pipeline_kwargs)
-    if response:
-        print(f"Instruction: {instruction}\n\n{response}\n\n-----------\n")
+data_files = [f for f in listdir(raw_dataset) if (isfile(join(raw_dataset, f)))]
+
+rejection_files = [f for f in data_files if f.endswith("_rejection.txt")]
+
+for f in rejection_files:
+        print(f)
+        with open(join(raw_dataset, f)) as r: print(instruct_pipeline("I received a denial: " + r.read() + ". Write an appeal to this denial"))
+        print(f)
+        with open(join(raw_dataset, f)) as r: print(old_instruct_pipeline("I received a denial: " + r.read() + ". Write an appeal to this denial"))
