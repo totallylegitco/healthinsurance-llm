48cfa737b979d54e6a621fd2dff4fa5fcaa51118
Holden Karau <holden@pigscanfly.ca>
2023-11-05 00:37:10 -0700

    Style fixes.

--- dataset_tools/ca_data.py
+++ dataset_tools/ca_data.py
@@ -3,10 +3,7 @@ import random
 import backoff
 import time
 import os
-from transformers import (
-    AutoModelForCausalLM,
-    AutoTokenizer,
-    pipeline)
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
 from os.path import join
 import pandas
 import torch
@@ -21,51 +18,64 @@ flatten = itertools.chain.from_iterable
 gen_loc = "generated-llm-data"
 
 parser = argparse.ArgumentParser(
-    prog='CA Data Generator',
-    description='Generate data',
-    epilog='Magic')
+    prog="CA Data Generator", description="Generate data", epilog="Magic"
+)
 
-parser.add_argument(
-    '--small-gpu',
-    action='store_true')
+parser.add_argument("--small-gpu", action="store_true")
 
 args = parser.parse_args()
 
 treatment_regex = re.compile(
     r"""\s*(The|An|A)?\s*(parent|father|mother|patient|enrollee|member|provider)\s*[^.]*(requested|required|asked|requires|reimbursement|coverage|requesting|has)\s*[^.]*(of|for|medication|reimbursement|coverage|services)\s+(?P<treatment>\d*\w+.+?)\.""",
-    re.IGNORECASE)
+    re.IGNORECASE,
+)
 alt_treatment_regex = re.compile(
     r"""At issue\s*(in this case|)\s*(is|)\s*(whether|if)\s+(?P<treatment>\d*\w+.+?) (is|were|was) medically (necessary|indicated)""",
-    re.IGNORECASE)
+    re.IGNORECASE,
+)
 more_alt_treatment_regex = re.compile(
     r"""the requested (medication|treatment|service|procedure)\s+(?P<treatment>\d*\w+.+?) (is|were|was) (likely to be|medically necessary|medically indicated)""",
-    re.IGNORECASE)
+    re.IGNORECASE,
+)
 
 even_more_alt_treatment_regex = re.compile(
     r"""(Therefore|Thus|As such),[^.]*?\s+(an|a|the|that) (?P<treatment>\w+[^.]+?) (is|were|was|should be) (medically necessary|medically indicated|likely to be|authorized)""",
-    re.IGNORECASE)
+    re.IGNORECASE,
+)
 
 perscribed_regex = re.compile(
-    r"""patients provider has prescribed the medication\s+(?P<treatment>[^.]+?).""", re.IGNORECASE)
+    r"""patients provider has prescribed the medication\s+(?P<treatment>[^.]+?).""",
+    re.IGNORECASE,
+)
 
 wishes_to_regex = re.compile(
-    r"""(wishes|desires|like) to (undergo|take)\s+(?P<treatment>[^.]+?).""", re.IGNORECASE)
+    r"""(wishes|desires|like) to (undergo|take)\s+(?P<treatment>[^.]+?).""",
+    re.IGNORECASE,
+)
 
 health_plan_not_necessary_regex = re.compile(
-    r"""The (Health Plan|Plan|Insurance Company) (determined the|determined|indicates) (?P<treatment>.+?) (is|was|were) not""", re.IGNORECASE)
+    r"""The (Health Plan|Plan|Insurance Company) (determined the|determined|indicates) (?P<treatment>.+?) (is|was|were) not""",
+    re.IGNORECASE,
+)
 
 almost_sketchy_regex = re.compile(
-    r"""treatment[^.]*with\s+(?P<treatment>[^.]+?) (is|were|was)""", re.IGNORECASE)
+    r"""treatment[^.]*with\s+(?P<treatment>[^.]+?) (is|were|was)""", re.IGNORECASE
+)
 
-sketchy_regex = re.compile(r"""(requested|required|asked|requires|reimbursement|coverage|request|requesting)\s*[^.]*(for|medication|reimbursement|coverage|of)\s+(?P<treatment>\d*\w+.+?)\.""",
-                           re.IGNORECASE)
+sketchy_regex = re.compile(
+    r"""(requested|required|asked|requires|reimbursement|coverage|request|requesting)\s*[^.]*(for|medication|reimbursement|coverage|of)\s+(?P<treatment>\d*\w+.+?)\.""",
+    re.IGNORECASE,
+)
 
 seeking_regex = re.compile(
-    r"""is seeking (?P<treatement>) for (?P<diagnosis>[^.]+)""", re.IGNORECASE)
+    r"""is seeking (?P<treatement>) for (?P<diagnosis>[^.]+)""", re.IGNORECASE
+)
 admitted_regex = re.compile(
-    r"""admitted to the hospital for (?P<diagnosis>[^.]+)""", re.IGNORECASE)
+    r"""admitted to the hospital for (?P<diagnosis>[^.]+)""", re.IGNORECASE
+)
 recommended_regex = re.compile(
-    r"""physicians recommended (?P<treatment>[^.]+)""", re.IGNORECASE)
+    r"""physicians recommended (?P<treatment>[^.]+)""", re.IGNORECASE
+)
 
 treatment_regexes = [
     treatment_regex,
@@ -77,7 +87,8 @@ treatment_regexes = [
     almost_sketchy_regex,
     sketchy_regex,
     seeking_regex,
-    recommended_regex]
+    recommended_regex,
+]
 
 diagnosis_regexes = [
     seeking_regex,
@@ -119,18 +130,26 @@ with open("bad_appeal_strings.txt") as f:
 def load_data(path):
     imr = pandas.read_csv(
         path,
-        usecols=["Determination", "TreatmentCategory", "TreatmentSubCategory",
-                 "DiagnosisCategory", "DiagnosisSubCategory", "Type", "Findings",
-                 "ReferenceID"
-                 ],
-        dtype=str)
+        usecols=[
+            "Determination",
+            "TreatmentCategory",
+            "TreatmentSubCategory",
+            "DiagnosisCategory",
+            "DiagnosisSubCategory",
+            "Type",
+            "Findings",
+            "ReferenceID",
+        ],
+        dtype=str,
+    )
 
     filtered_imr = imr[imr["Determination"].str.contains("Overturned")]
     return filtered_imr
 
 
 imrs = load_data(
-    "./data_sources/ca-independent-medical-review-imr-determinations-trends-utf8.csv")
+    "./data_sources/ca-independent-medical-review-imr-determinations-trends-utf8.csv"
+)
 
 
 def generate_prompts(imr):
@@ -140,56 +159,52 @@ def generate_prompts(imr):
     findings = imr["Findings"].strip("\n")
     grounds = imr["Type"]
     index = imr["ReferenceID"]
-    
+
     prompts = {
-        "denial": [f"""The independent medical review findings were {findings} and grounds were {grounds}. Use this information to write the original insurance denial."""],
-        "appeal": [f"""The independent medical review findings were {findings} and grounds were {grounds}. Use this information to write the original appeal by the patient."""]
+        "denial": [
+            f"""The independent medical review findings were {findings} and grounds were {grounds}. Use this information to write the original insurance denial."""
+        ],
+        "appeal": [
+            f"""The independent medical review findings were {findings} and grounds were {grounds}. Use this information to write the original appeal by the patient."""
+        ],
     }
 
     return (index, prompts)
 
 
 def work_with_generative_remote():
-    @backoff.on_exception(backoff.expo,
-                          requests.exceptions.RequestException,
-                          max_time=600)
+    @backoff.on_exception(
+        backoff.expo, requests.exceptions.RequestException, max_time=600
+    )
     def make_request(model, prompt):
         url = "https://api.perplexity.ai/chat/completions"
 
         token = os.getenv("PERPLEXITY_API")
         if token is None:
             print("Error no Token provided for perplexity.")
-        
+
         payload = {
             "model": model,
             "messages": [
-                {
-                    "role": "system",
-                    "content": "Be precise and concise."
-                },
-                {
-                    "role": "user",
-                    "content": prompt
-                }
-            ]
+                {"role": "system", "content": "Be precise and concise."},
+                {"role": "user", "content": prompt},
+            ],
         }
         headers = {
             "accept": "application/json",
             "content-type": "application/json",
-            "Authorization": f"Bearer {token}"
+            "Authorization": f"Bearer {token}",
         }
 
-        time.sleep(random.randint(0,10))
+        time.sleep(random.randint(0, 10))
         response = requests.post(url, json=payload, headers=headers)
         response.raise_for_status()
 
         response_text = response.json()["choices"][0]["message"]["content"]
         print(f"Promxspt: {prompt}\nResponse text: {response_text}")
         return response_text
-    models = [
-        "mistral-7b-instruct",
-        "openhermes-2-mistral-7b"
-    ]
+
+    models = ["mistral-7b-instruct", "openhermes-2-mistral-7b"]
 
     print("Generating prompts...")
     l = imrs.apply(generate_prompts, axis=1).tolist()
@@ -210,8 +225,10 @@ def work_with_generative_remote():
                                 with open(join(gen_loc, target_file), "w") as f:
                                     f.write(r)
                             else:
-                                print(f"Skipping, found bad data in {r} for rt {response_type}")
-                        
+                                print(
+                                    f"Skipping, found bad data in {r} for rt {response_type}"
+                                )
+
 
 def work_with_generative_local():
     # Load the model to do our magic
@@ -242,9 +259,9 @@ def work_with_generative_local():
                     eos_token_id=tokenizer.eos_token_id,
                     torch_dtype=torch.bfloat16,
                     trust_remote_code=True,
-                    model_kwargs={'load_in_8bit': True},
+                    model_kwargs={"load_in_8bit": True},
                     device_map="auto",
-                    max_new_tokens=1124
+                    max_new_tokens=1124,
                 )
             else:
                 instruct_pipeline = pipeline(
@@ -255,7 +272,8 @@ def work_with_generative_local():
                     torch_dtype=torch.bfloat16,
                     trust_remote_code=True,
                     device_map="auto",
-                    max_new_tokens=1124)
+                    max_new_tokens=1124,
+                )
             break
         except Exception as e:
             print(f"Error {e} loading {model}")
@@ -263,21 +281,20 @@ def work_with_generative_local():
     if instruct_pipeline is None:
         raise Exception("Could not load any model")
 
-
     print("Generating prompts...")
     l = imrs.apply(generate_prompts, axis=1).tolist()
     batch_size = 20
 
     for b in range(0, len(l), batch_size):
         print(f"Running batch {b}")
-        batch = l[b: b + batch_size]
+        batch = l[b : b + batch_size]
 
         for k in batch[0].keys():
             mybatch = list(map(lambda x: x[k], batch))
             c = 0
             start_idxs = []
             prompts = []
-            for (idx, batch_prompts) in mybatch:
+            for idx, batch_prompts in mybatch:
                 start_idxs += [c]
                 c = c + len(batch_prompts)
                 prompts += batch_prompts
@@ -292,18 +309,20 @@ def work_with_generative_local():
             print(f"Got back {len(results)}")
             ci = 0
 
-            for (idx, batch_prompts) in mybatch:
+            for idx, batch_prompts in mybatch:
                 start = start_idxs[ci]
                 ci = ci + 1
                 i = 0
-                local_results = results[start: start + len(batch_prompts)]
+                local_results = results[start : start + len(batch_prompts)]
                 for r in local_results:
                     if r is None:
                         continue
                     i = i + 1
                     if not check_for_bad(response_type, r):
                         print(f"Writing out to {idx}MAGIC{i}{response_type}.txt")
-                        with open(join(gen_loc, f"{idx}MAGIC{i}{response_type}.txt"), "w") as f:
+                        with open(
+                            join(gen_loc, f"{idx}MAGIC{i}{response_type}.txt"), "w"
+                        ) as f:
                             f.write(r)
                     else:
                         print(f"Skipping, found bad data in {r} for rt {response_type}")
@@ -311,7 +330,8 @@ def work_with_generative_local():
 
 def work_with_biogpt():
     instruct_pipeline = pipeline(
-        model="microsoft/BioGPT-Large-PubMedQA", max_new_tokens=200)
+        model="microsoft/BioGPT-Large-PubMedQA", max_new_tokens=200
+    )
 
     def generate_biogpt_hacks(imr):
         treatment = get_treatment_from_imr(imr)
@@ -321,7 +341,9 @@ def work_with_biogpt():
         if treatment is None:
             return None
         if diagnosis is not None:
-            return [(index, f"{treatment} is medically necessary for {diagnosis} because")]
+            return [
+                (index, f"{treatment} is medically necessary for {diagnosis} because")
+            ]
         else:
             return [(index, f"{treatment} is medically necessary because")]
 
@@ -330,9 +352,11 @@ def work_with_biogpt():
         reason = extract_text(res[1])
         print(res)
         with open(join(gen_loc, f"{idx}MAGICB_appeal.txt"), "w") as o:
-            o.write("""Dear [InsuranceCompany];
+            o.write(
+                """Dear [InsuranceCompany];
 
-I am writing you to appeal claim [CLAIMNUMBER]. I believe that it is medically necessary.""")
+I am writing you to appeal claim [CLAIMNUMBER]. I believe that it is medically necessary."""
+            )
             o.write(reason)
             o.write("\n")
             o.write("Sincerely,\n[YOURNAME]")
@@ -341,10 +365,10 @@ I am writing you to appeal claim [CLAIMNUMBER]. I believe that it is medically n
 
     for i in range(0, len(imrs), batch_size):
         print(f"looping on batch {i}")
-        batch = imrs[i: i + batch_size]
-        batch_prompts = list(filter(
-            not_none,
-            batch.apply(generate_biogpt_hacks, axis=1).tolist()))
+        batch = imrs[i : i + batch_size]
+        batch_prompts = list(
+            filter(not_none, batch.apply(generate_biogpt_hacks, axis=1).tolist())
+        )
         idxs = map(lambda r: r[0], batch)
         qs = map(lambda r: r[1], batch)
         transformed = instruct_pipeline(list(qs))
