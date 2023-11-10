52bf10e7dde40ff939167e96465c2998f0d749fe
Holden Karau <holden@pigscanfly.ca>
2023-11-09 20:28:17 -0800

    Install correct torch first

--- firstrun.sh
+++ firstrun.sh
@@ -13,6 +13,10 @@ if [ ! -f ".firstrun" ]; then
   # We need to install pybind11 before deepspeed because it is not listed as a depdency.
   pip install pybind11[global]
   pip install packaging
+  # We need good pytorch v soon
+  CU_MINOR=$(nvcc --version |grep "cuda_" |cut -d "_" -f 2 |cut -d "." -f 2)
+  pip install "torch" --index-url https://download.pytorch.org/whl/cu11${CU_MINOR} || pip install torch
+
   if [ ! -d axolotl ]; then
     git clone https://github.com/OpenAccess-AI-Collective/axolotl
   fi
@@ -29,8 +33,6 @@ if [ ! -f ".firstrun" ]; then
   cd ..
 
   # deepspeed
-  CU_MINOR=$(nvcc --version |grep "cuda_" |cut -d "_" -f 2 |cut -d "." -f 2)
-  pip install "torch" --index-url https://download.pytorch.org/whl/cu11${CU_MINOR} || pip install torch
   pip install ninja hjson py-cpuinfo
   DS_BUILD_CPU_ADAM=1 DS_BUILD_SPARSE_ATTN=0 DS_BUILD_FUSED_ADAM=1 pip install "git+https://github.com/microsoft/deepspeed.git#" --global-option="build_ext" --global-option="-j16"
   pip3 install -U -r requirements.txt --extra-index-url https://download.pytorch.org/whl/cu11${CU_MINOR}
