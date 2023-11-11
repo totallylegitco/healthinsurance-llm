cf3539e25dba23edef55cd87cf6c7ae92ec750a8
Holden Karau <holden@pigscanfly.ca>
2023-11-11 10:36:26 -0800

    Add port for runpod

--- run_remote.sh
+++ run_remote.sh
@@ -3,6 +3,7 @@
 INPUT_MODEL=${INPUT_MODEL:-"databricks/dolly-v2-7b"}
 
 HOST=$1
+PORT={$2:-22}
 
 set -ex
 
@@ -22,26 +23,26 @@ tpid=$!
 
 # Copy over firstrun.sh so we can get the system setup a bit while we transfer all of ourdata.
 # Lambda labs seems to be having isssues with ssh not coming up quickly so retry.
-(scp ./firstrun.sh $HOST:~/ || (sleep 120 && scp ./firstrun.sh $HOST:~/ ))
-scp ./requirements.txt $HOST:~/
-ssh $HOST "./firstrun.sh | tee -a fr.log" &
+(scp -P $PORT ./firstrun.sh $HOST:~/ || (sleep 120 && scp -P $PORT ./firstrun.sh $HOST:~/ ))
+scp -P $PORT ./requirements.txt $HOST:~/
+ssh -p $PORT $HOST "./firstrun.sh | tee -a fr.log" &
 frpid=$!
 # Put the passwordless https config there
-scp ~/.ssh/authorized_keys  $HOST:~/.ssh/
-# ssh -t $HOST "sudo apt-get update && sudo apt-get upgrade -y" &
+scp -P $PORT ~/.ssh/authorized_keys  $HOST:~/.ssh/
+# ssh -p $PORT -t $HOST "sudo apt-get update && sudo apt-get upgrade -y" &
 wait ${tpid}
 # Race condition with tbz2 file not being written all the way
 sync
 sleep 1
-scp ${target} $HOST:~/
-ssh $HOST "tar -C ./ -xjf ${filename}" &
+scp -P $PORT ${target} $HOST:~/
+ssh -p $PORT $HOST "tar -C ./ -xjf ${filename}" &
 wait $!
-scp remote_git $HOST:~/.git/config
+scp -P $PORT remote_git $HOST:~/.git/config
 # Copy git credentials for huggingface access.
-scp ~/.gitconfig $HOST:~/.gitconfig
-scp ~/.config/git/credentials $HOST:~/.config/git/credentials || scp ~/.git-credentials $HOST:~/
-ssh $HOST mkdir -p "~/.cache/huggingface"
-scp ~/.cache/huggingface/token $HOST:~/.cache/huggingface/token
+scp -P $PORT ~/.gitconfig $HOST:~/.gitconfig
+scp -P $PORT ~/.config/git/credentials $HOST:~/.config/git/credentials || scp -P $PORT ~/.git-credentials $HOST:~/
+ssh -p $PORT $HOST mkdir -p "~/.cache/huggingface"
+scp -P $PORT ~/.cache/huggingface/token $HOST:~/.cache/huggingface/token
 wait ${frpid}
-ssh -t $HOST "QLORA=\"${QLORA}\" INPUT_MODEL=\"${INPUT_MODEL}\" screen ./run.sh"
+ssh -p $PORT -t $HOST "QLORA=\"${QLORA}\" INPUT_MODEL=\"${INPUT_MODEL}\" screen ./run.sh"
 rm -rf ${temp_dir}
