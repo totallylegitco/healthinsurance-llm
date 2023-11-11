b7d245375b0e2b67eac27c1b924985ffb9f3570e
Holden Karau <holden@pigscanfly.ca>
2023-11-11 11:06:45 -0800

    Try and add target (for our friend runpod which has persistent storage magics).

--- run_remote.sh
+++ run_remote.sh
@@ -4,6 +4,7 @@ INPUT_MODEL=${INPUT_MODEL:-"databricks/dolly-v2-7b"}
 
 HOST=${1:-${HOST}}
 PORT=${2:-${PORT:-22}}
+TARGET=${3:-${TARGET:-~/}}
 
 set -ex
 
@@ -23,19 +24,19 @@ tpid=$!
 
 # Copy over firstrun.sh so we can get the system setup a bit while we transfer all of ourdata.
 # Lambda labs seems to be having isssues with ssh not coming up quickly so retry.
-(scp -P $PORT ./firstrun.sh $HOST:~/ || (sleep 120 && scp -P $PORT ./firstrun.sh $HOST:~/ ))
-scp -P $PORT ./requirements.txt $HOST:~/
+(scp -P $PORT ./firstrun.sh $HOST:${TARGET} || (sleep 120 && scp -P $PORT ./firstrun.sh $HOST:${TARGET} ))
+scp -P $PORT ./requirements.txt $HOST:${TARGET}
 ssh -p $PORT $HOST "./firstrun.sh | tee -a fr.log" &
 frpid=$!
 # Put the passwordless https config there
-scp -P $PORT ~/.ssh/authorized_keys  $HOST:~/.ssh/
+# scp -P $PORT ~/.ssh/authorized_keys  $HOST:~/.ssh/
 # ssh -p $PORT -t $HOST "sudo apt-get update && sudo apt-get upgrade -y" &
 wait ${tpid}
 # Race condition with tbz2 file not being written all the way
 sync
 sleep 1
-scp -P $PORT ${target} $HOST:~/
-ssh -p $PORT $HOST "tar -C ./ -xjf ${filename}" &
+scp -P $PORT ${target} $HOST:${TARGET}
+ssh -p $PORT $HOST "tar -C ${TARGET} -xjf ${TARGET}${filename}" &
 wait $!
 scp -P $PORT remote_git $HOST:~/.git/config
 # Copy git credentials for huggingface access.
@@ -44,5 +45,5 @@ scp -P $PORT ~/.config/git/credentials $HOST:~/.config/git/credentials || scp -P
 ssh -p $PORT $HOST mkdir -p "~/.cache/huggingface"
 scp -P $PORT ~/.cache/huggingface/token $HOST:~/.cache/huggingface/token
 wait ${frpid}
-ssh -p $PORT -t $HOST "QLORA=\"${QLORA}\" INPUT_MODEL=\"${INPUT_MODEL}\" screen ./run.sh"
+ssh -p $PORT -t $HOST "QLORA=\"${QLORA}\" INPUT_MODEL=\"${INPUT_MODEL}\" screen ${TARGET}/run.sh"
 rm -rf ${temp_dir}
