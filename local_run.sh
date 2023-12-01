88ae7d4061d64ffb03405074a0ed2243fc8a210e
Holden Karau <holden@pigscanfly.ca>
2023-12-01 11:22:11 -0800

    local run script

---
+++ local_run.sh
@@ -0,0 +1,1 @@
+docker run -d --gpus all     -p 8000:8000     -v  /home/holden/repos/healthinsurance-llm/fighthealthinsurance_model_v0.2:/fighthealthinsurance_model_v0.2 ghcr.io/mistralai/mistral-src/vllm:latest     --host 0.0.0.0     --model="/fighthealthinsurance_model_v0.2"
