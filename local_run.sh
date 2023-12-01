ee4b30f140f659acb3dc1293c2456da99d893ae0
Holden Karau <holden@pigscanfly.ca>
2023-12-01 11:45:16 -0800

    Change ze port forward.

--- local_run.sh
+++ local_run.sh
@@ -1,1 +1,2 @@
-docker run -d --gpus all     -p 8000:8000     -v  /home/holden/repos/healthinsurance-llm/fighthealthinsurance_model_v0.2:/fighthealthinsurance_model_v0.2 ghcr.io/mistralai/mistral-src/vllm:latest     --host 0.0.0.0     --model="/fighthealthinsurance_model_v0.2"
+PORT=${PORT:-8000}
+docker run -d --gpus all     -p ${PORT}:8000     -v  /home/holden/repos/healthinsurance-llm/fighthealthinsurance_model_v0.2:/fighthealthinsurance_model_v0.2 ghcr.io/mistralai/mistral-src/vllm:latest     --host 0.0.0.0     --model="/fighthealthinsurance_model_v0.2"
