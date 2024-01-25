4b83f10c56aa036a3b1969f6c41d0e61d06ebf64
Holden Karau <holden@pigscanfly.ca>
2024-01-24 20:03:02 -0800

    More cleanup.

--- dataset_tools/ca_data.py
+++ dataset_tools/ca_data.py
@@ -168,7 +168,7 @@ def generate_prompts(imr):
             f"""The independent medical review findings were {findings} and grounds were {grounds}{treatment_extra}. Use this information to write the original insurance denial as if you were a non-practicing doctor responding on behalf of an insurance company. Do not include any reference to the reviewers or their findings, instead focus on what the insurance company would have written denying the patients first claim. Feel free to be verbose and start your denial with \"Dear [Member];\"""",
         ],
         "appeal": [
-            f"""The independent medical review findings were {findings} and grounds were {grounds}{treatment_extra}. In your response You are writing on your own on behalf (not that of a doctors office) and you do not have any credentials. Do not include any reference to the reviewers or their findings. Use this information to write the original appeal by the patient. Feel free to be verbose and start your appeal with Dear [Insurance Company];""",
+            f"""The independent medical review findings were {findings} and grounds were {grounds}{treatment_extra}. In your response you are writing on your own on behalf (not that of a doctors office) and you do not have any credentials. Do not include any reference to the reviewers or their findings. Use this information to write the original appeal by the patient. Feel free to be verbose and start your appeal with Dear [Insurance Company];""",
         ],
         "medically_necessary": [
             f"""The independent medical review findings were {findings} and grounds were {grounds}{treatment_extra}. Why was the treatment considered medically necessary? Don't refer to the reviewers findings directly instead write in a general fashion."""
