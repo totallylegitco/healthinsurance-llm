07a9e1a42a7634867cdd9aa976245cab662b6aeb
Holden Karau <holden@pigscanfly.ca>
2024-02-10 13:14:43 -0800

    Ok do the thing

--- rag/create.sh
+++ rag/create.sh
@@ -3,3 +3,7 @@
 export OPENAI_API_KEY="fake"
 export LOCAL_MODEL="/TotallyLegitCo/fighthealthinsurance_model_v0.3"
 export OPENAI_BASE_API="http://localhost:8000"
+
+set -ex
+
+python -m rag.initial
