6285d5cf700232a14aea5848986b6984be131f4f
Holden Karau <holden@pigscanfly.ca>
2024-02-10 12:10:18 -0800

    Fix launching of local model.

--- local_run.sh
+++ local_run.sh
@@ -1,2 +1,2 @@
 PORT=${PORT:-8000}
-docker run -d --gpus all     -p ${PORT}:8000     -v  /home/holden/repos/healthinsurance-llm/fighthealthinsurance_model_v0.3:TotallyLegitCo/fighthealthinsurance_model_v0.3 fighthealthinsurance_model_v0.3 vllm/vllm-openai:latest     --host 0.0.0.0  --gpu-memory-utilization=0.9 --max-model-len=26280 --enforce-eager   --model="TotallyLegitCo/fighthealthinsurance_model_v0.3"
+docker run -d --gpus all     -p ${PORT}:8000     -v  /home/holden/repos/healthinsurance-llm/fighthealthinsurance_model_v0.3:/TotallyLegitCo/fighthealthinsurance_model_v0.3 vllm/vllm-openai:latest     --host 0.0.0.0  --gpu-memory-utilization=0.9 --max-model-len=26280 --enforce-eager   --model="/TotallyLegitCo/fighthealthinsurance_model_v0.3"
