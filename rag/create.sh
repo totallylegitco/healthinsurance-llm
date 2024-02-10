06041265746e291641c66e615ca226129b98a47e
Holden Karau <holden@pigscanfly.ca>
2024-02-10 14:03:42 -0800

    File path

--- rag/create.sh
+++ rag/create.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export OPENAI_API_KEY="EMPTY"
-export LOCAL_MODEL="/TotallyLegitCo/fighthealthinsurance_model_v0.3"
+export LOCAL_MODEL="./TotallyLegitCo/fighthealthinsurance_model_v0.3"
 export OPENAI_BASE_API="http://localhost:8000/v1"
 
 export |grep OPENAI_BASE_API
