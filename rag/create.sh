6b5070cc6eb758f80e3ea43d2a4e556cb3924623
Holden Karau <holden@pigscanfly.ca>
2024-02-10 13:25:47 -0800

    hmm this is weird

--- rag/create.sh
+++ rag/create.sh
@@ -4,6 +4,8 @@ export OPENAI_API_KEY="EMPTY"
 export LOCAL_MODEL="/TotallyLegitCo/fighthealthinsurance_model_v0.3"
 export OPENAI_BASE_API="http://localhost:8000/v1"
 
+export |grep OPENAI_BASE_API
+
 set -ex
 
 python -m rag.initial
